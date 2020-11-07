1688263de719fca63b168b513a536ebd18c29d45
Lichthagel <lichthagel@tuta.io>
2020-11-07 14:28:22 +0100

    Add RateEntry struct and read_data method

---
+++ src/entry_struct.rs
@@ -0,0 +1,11 @@
+pub struct RateEntry {
+    name: String,
+    wins: u32,
+    votes: u32,
+}
+
+impl RateEntry {
+    pub fn new(name: String, wins: u32, votes: u32) -> RateEntry {
+        RateEntry { name, wins, votes }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod entry_struct;
+
+use crate::entry_struct::RateEntry;
 use clap::App;
 use std::{
     fs::File,
@@ -18,4 +21,8 @@ fn read_lines<P: AsRef<Path>>(file: P) -> Result<Vec<String>, io::Error> {
     let reader = BufReader::new(f);
 
     reader.lines().collect()
+}
+
+fn read_data<P: AsRef<Path>>(file: P) -> Result<Vec<RateEntry>, io::Error> {
+    read_lines(file).map(|v| v.into_iter().map(|s| RateEntry::new(s, 0, 0)).collect())
 }
